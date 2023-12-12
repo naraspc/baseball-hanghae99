2e66b1b4f3d84e00ad726946083ba9844e0c4906
naraspc <wodnrtkfka@naver.com>
2023-12-12 20:04:48 +0900

    feat add OutputView
    아웃풋 뷰 추가

--- src/main/java/com/baseball/View/OutputView.java
+++ src/main/java/com/baseball/View/OutputView.java
@@ -2,17 +2,25 @@ package com.baseball.View;
 
 public class OutputView {
     private static final String GAME_START = "컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!";
+    private static final String GAME_TRY_COUNT = "번째 시도 : ";
     private static final String GAME_END = "게임을 종료합니다.";
     private static final String GAME_END_TRY = "번만에 맞히셧습니다.";
 
-    public static void  GameStart() {
+
+    public void GameStart() {
         System.out.println(GAME_START);
     }
-    public static void GameEnd() {
+
+    public void GameEnd() {
         System.out.println(GAME_END);
     }
-    public static void gameCount() {
+
+    public void gameCount() {
         System.out.println(GAME_END_TRY);
+    }
+
+    public void tryCount() {
+        System.out.println(GAME_TRY_COUNT);
     }
 
 }
